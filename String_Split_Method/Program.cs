96ef5cbfcc21e978a204a524d6f991852632041c
BackSeungJoo <lueun59@gmail.com>
2023-06-19 20:19:41 +0900

    📰 Init: "문자열 분할 예제"
    // Split() - 지정된 문자를 기준으로 현재 문자열을 분리한 다음 분리한 문자열의 배열을 반환
    // SubString() - 현재 문자열의 지정된 위치로부터 지정된 수만큼의 문자로 이루어진 새 문자열을 반환

---
+++ String_Split_Method/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Split_Method
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String greeting = "Good morning.";
+
+            // 0 부터 시작해서 길이가 5인 문자열을 반환
+            WriteLine(greeting.Substring(0, 5)); // "Good"
+
+            // 5부터 끝까지의 문자열을 반환
+            WriteLine(greeting.Substring(5));    // "morning"
+            WriteLine();
+
+            // greeting 문자열을 공백을 기준으로 분리합니다.
+            string[] arr = greeting.Split(new string[] { " " }, StringSplitOptions.None);
+            WriteLine("Word Count : {0}", arr.Length);
+
+            // arr 배열의 각 요소를 출력합니다.
+            foreach(string element in arr)
+            {
+                WriteLine("{0}", element);
+            }
+        }
+    }
+}
