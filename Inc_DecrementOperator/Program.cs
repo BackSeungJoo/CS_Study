737805e5b060c67f12a6ea61da0219a6fa69beb7
BackSeungJoo <lueun59@gmail.com>
2023-06-20 13:32:51 +0900

    📰 Init: "증감연산자"

---
+++ Inc_DecrementOperator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inc_DecrementOperator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 증가 연산자
+            int a = 10;
+            Console.WriteLine(a++);
+            Console.WriteLine(++a);
+
+            // 감소 연산자
+            Console.WriteLine(a--);
+            Console.WriteLine(--a);
+        }
+    }
+}
