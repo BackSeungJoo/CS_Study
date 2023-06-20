44c8b5efe60dae74f2dea41b7f1ab0a5d7607418
BackSeungJoo <lueun59@gmail.com>
2023-06-20 13:57:10 +0900

    📰 Init: "논리 연산자"
    // and (논리곱), or (논리합) , not (부정)

---
+++ Logical_Operator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logical_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // and 연산 (논리곱)
+            Console.WriteLine("Testing && ... ");
+            Console.WriteLine($"1 > 0 && 4 < 5 : {1 > 0 && 4 < 5}");
+            Console.WriteLine($"1 > 0 && 4 > 5 : {1 > 0 && 4 > 5}");
+            Console.WriteLine($"1 == 0 && 4 > 5 : {1 == 0 && 4 > 5}");
+            Console.WriteLine($"1 == 0 && 4 < 5 : {1 == 0 && 4 < 5}");
+            Console.WriteLine();
+
+            // or 연산 (논리합)
+            Console.WriteLine("Testing || ... ");
+            Console.WriteLine($"1 > 0 || 4 < 5 : {1 > 0 || 4 < 5}");
+            Console.WriteLine($"1 > 0 || 4 > 5 : {1 > 0 || 4 > 5}");
+            Console.WriteLine($"1 == 0 || 4 > 5 : {1 == 0 || 4 > 5}");
+            Console.WriteLine($"1 == 0 || 4 < 5 : {1 == 0 || 4 < 5}");
+            Console.WriteLine();
+
+            // not 연산
+            Console.WriteLine("Testing ! ... ");
+            Console.WriteLine($"!True : {!true}");
+            Console.WriteLine($"!False : {!false}");
+            Console.WriteLine();
+        }
+    }
+}
