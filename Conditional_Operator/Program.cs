71d6e44202250e8fcf33b286219de415a32c1470
BackSeungJoo <lueun59@gmail.com>
2023-06-20 14:22:47 +0900

    📰 Init: "조건 연산자"
    // 삼항 연산자
    // 홀짝 계산

---
+++ Conditional_Operator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conditional_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 홀짝 계산
+            string result = (10 % 2) == 0 ? "짝수" : "홀수";
+            Console.WriteLine(result);
+        }
+    }
+}
