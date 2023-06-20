e82bd5261a96bbe45cf26761301e621749086f1b
BackSeungJoo <lueun59@gmail.com>
2023-06-20 14:58:03 +0900

    📰 Init: "비트 논리 연산자"
    and  &
    or      |
    xor    ^
    not   ~

---
+++ Bit_Wise_Operator/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bit_Wise_Operator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 9;
+            int b = 10;
+
+            // 비트 and 연산
+            Console.WriteLine($"{a} & {b} : {a & b}");
+            // 비트 or 연산
+            Console.WriteLine($"{a} | {b} : {a | b}");
+            // 비트 xor 연산
+            Console.WriteLine($"{a} ^ {b} : {a ^ b}");
+
+            // 비트 not 연산
+            int c = 255;
+            Console.WriteLine("~{0}(0x{0:X8}) : {1}(0x{1:X8})", c, ~c);
+        }
+    }
+}
